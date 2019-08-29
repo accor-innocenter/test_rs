5c65a68ba9e7ad33ffc6527ee302ef5140deabe1
InnoCenter <innocenter@MacBook-Pro-7.local>
2019-08-29 13:08:35 +0200

    real flow

--- action-rsjs.js
+++ action-rsjs.js
@@ -1,8 +1,9 @@
 #! /usr/bin/env node
 
 
-const { withHermes } = require('hermes-javascript')
+const { withHermes } = require('hermes-javascript');
 
+const server = "http://192.168.86.76/";
 /*
     A small js context manager that sets up an infinite loop to prevent
     the process from exiting, and exposes an instance of the Hermes class.
@@ -15,9 +16,69 @@ withHermes(hermes => {
     dialog.flow('AccorInnovationCenter:OrderRS', (msg, flow) => {
         // Log intent message
         console.log(JSON.stringify(msg))
+    
+        flow.continue('AccorInnovationCenter:FirstCourse',(msg,flow) =>  {
+            console.log(JSON.stringify(msg))
+
+            flow.continue('AccorInnovationCenter:SecondCourse',(msg,flow) =>  {
+                console.log(JSON.stringify(msg))
+    
+                flow.continue('AccorInnovationCenter:Dessert',(msg,flow) =>  {
+                    console.log(JSON.stringify(msg))
+        
+                    let url = server + "domotics/fitness";
+                    var payload = { };
+                    var data = new FormData();
+                    data.append( "json", JSON.stringify( payload ) );
+                    fetch(url,
+                    {
+                        method: "GET",
+                        body: data
+                    })
+                    .then((res)=>{ 
+                        console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+                        console.log(res);
+                    })
+                    .then((data)=>{ 
+                        console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+                        console.log(data);
+                    })
+                    
+                    return msg.slots[0].value.value+" est une spécialité de la maison. Très bien, votre commande arrivera dans 20 minutes."
+        
+                });
+                
+                return msg.slots[0].value.value+", parfait. Et quel dessert souhaitez-vous déguster?"
+    
+            });
+
+            return msg.slots[0].value.value+", excellent choix. Quel sera votre plat principal?"
+
+        });
+
         // End the session
-        flow.end()
+        flow.end();
+
+
+        let url = server + "domotics/menu";
+        var payload = { };
+        var data = new FormData();
+        data.append( "json", JSON.stringify( payload ) );
+        fetch(url,
+        {
+            method: "GET",
+            body: data
+        })
+        .then((res)=>{ 
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+            console.log(res);
+        })
+        .then((data)=>{ 
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+            console.log(data);
+        })
+
         // Use text to speech
-        return 'Voici le menu';
+        return 'Voici le menu sur votre écran. Quelle entrée voulez-vous?';
     })
 })
