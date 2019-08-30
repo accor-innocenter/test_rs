16451f6d24d7b6435a4305b5884e3dc71c0d1328
cesarmiguel85 <cesarmiguel85@gmail.com>
2019-08-30 16:51:48 +0200

    Update action-rsjs.js

--- action-rsjs.js
+++ action-rsjs.js
@@ -83,38 +83,77 @@ function sayTTSawait(msg, lang) {
 
 }
 
-await withHermes(hermes => {
-    // Instantiate a dialog object
-    const dialog = hermes.dialog()
+
+async function RunMe() {
+    withHermes(hermes => {
+        // Instantiate a dialog object
+        const dialog = hermes.dialog()
 
 
-    // Subscribes to intent 'myIntent'
-    dialog.flow('AccorInnovationCenter:OrderRS', (msg, flow) => {
-        // Log intent message
-        console.log(JSON.stringify(msg))
+        // Subscribes to intent 'myIntent'
+        dialog.flow('AccorInnovationCenter:OrderRS', (msg, flow) => {
+            // Log intent message
+            console.log(JSON.stringify(msg))
 
 
-        flow.continue('AccorInnovationCenter:FirstCourse', (msg, flow) => {
-            console.log(JSON.stringify(msg));
+            flow.continue('AccorInnovationCenter:FirstCourse', (msg, flow) => {
+                console.log(JSON.stringify(msg));
 
-            const aux = msg.slots.find(slot => slot.slotName === 'Dish1');
-            this.FirstDish = aux.value.value;
+                const aux = msg.slots.find(slot => slot.slotName === 'Dish1');
+                this.FirstDish = aux.value.value;
 
-            flow.continue('AccorInnovationCenter:SecondCourse', (msg, flow) => {
-                console.log(JSON.stringify(msg))
-
-                const aux = msg.slots.find(slot => slot.slotName === 'Dish2');
-                this.SecondDish = aux.value.value;
-
-
-                flow.continue('AccorInnovationCenter:Dessert', (msg, flow) => {
+                flow.continue('AccorInnovationCenter:SecondCourse', (msg, flow) => {
                     console.log(JSON.stringify(msg))
 
-                    const aux = msg.slots.find(slot => slot.slotName === 'Dessert');
-                    this.Dessert = aux.value.value;
+                    const aux = msg.slots.find(slot => slot.slotName === 'Dish2');
+                    this.SecondDish = aux.value.value;
 
-                    /*
-                    let url = server + "domotics/fitness";
+
+                    flow.continue('AccorInnovationCenter:Dessert', (msg, flow) => {
+                        console.log(JSON.stringify(msg))
+
+                        const aux = msg.slots.find(slot => slot.slotName === 'Dessert');
+                        this.Dessert = aux.value.value;
+
+                        /*
+                        let url = server + "domotics/fitness";
+                        var payload = { };
+                        var data = new FormData();
+                        data.append( "json", JSON.stringify( payload ) );
+                        fetch(url,
+                        {
+                            method: "GET",
+                            body: data
+                        })
+                        .then((res)=>{ 
+                            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+                            console.log(res);
+                        })
+                        .then((data)=>{ 
+                            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+                            console.log(data);
+                        })
+    
+    */
+                        // End the session
+                        flow.end();
+
+
+                        return msg.slots[0].value.value + " est une spécialité de la maison. Très bien, votre commande arrivera dans 20 minutes."
+
+
+                    });
+
+                    return msg.slots[0].value.value + ", parfait. Et quel dessert souhaitez-vous déguster?"
+
+                });
+
+                return msg.slots[0].value.value + ", excellent choix. Quel sera votre plat principal?"
+
+            });
+
+            /*
+                    let url = server + "domotics/menu";
                     var payload = { };
                     var data = new FormData();
                     data.append( "json", JSON.stringify( payload ) );
@@ -131,81 +170,47 @@ await withHermes(hermes => {
                         console.log("@@@@@@@@@@@@@@@@@@@@@@@");
                         console.log(data);
                     })
+            */
 
-*/
-                    // End the session
-                    flow.end();
-
-
-                    return msg.slots[0].value.value + " est une spécialité de la maison. Très bien, votre commande arrivera dans 20 minutes."
-
-
-                });
-
-                return msg.slots[0].value.value + ", parfait. Et quel dessert souhaitez-vous déguster?"
-
+            const fetchPromise = fetch(server + "domotics/menu");
+            fetchPromise.then(response => {
+                console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+                console.log(response);
+                return response.json();
+            }).then(data => {
+                console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+                console.log(data);
             });
 
-            return msg.slots[0].value.value + ", excellent choix. Quel sera votre plat principal?"
+            // Use text to speech
 
-        });
-
-        /*
-                let url = server + "domotics/menu";
-                var payload = { };
-                var data = new FormData();
-                data.append( "json", JSON.stringify( payload ) );
-                fetch(url,
-                {
-                    method: "GET",
-                    body: data
-                })
-                .then((res)=>{ 
-                    console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                    console.log(res);
-                })
-                .then((data)=>{ 
-                    console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                    console.log(data);
-                })
-        */
-
-        const fetchPromise = fetch(server + "domotics/menu");
-        fetchPromise.then(response => {
-            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-            console.log(response);
-            return response.json();
-        }).then(data => {
-            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-            console.log(data);
-        });
-
-        // Use text to speech
-
-        flow.end();
-        /*
-                sayTTS("Ceci est un test de TTS", "fr")
-                    .then((data) => {
-                        console.log("C'est bon: " + data);
-
-                        sayTTS("Deuxième message à la suite", "fr").then((data) => {
-                            console.log("C'est bon 2e: " + data);
-                            setTimeout(function() {
-                                sayTTS("Troisième message 5 secondes après.")
-                            }, 5000)
+            flow.end();
+            /*
+                    sayTTS("Ceci est un test de TTS", "fr")
+                        .then((data) => {
+                            console.log("C'est bon: " + data);
+    
+                            sayTTS("Deuxième message à la suite", "fr").then((data) => {
+                                console.log("C'est bon 2e: " + data);
+                                setTimeout(function() {
+                                    sayTTS("Troisième message 5 secondes après.")
+                                }, 5000)
+                            })
+    
                         })
+                        .catch((error) => {
+                            console.log("Erreur: " + error)
+                            return 'Il y a eu une erreur';
+                        })
+            */
+            await sayTTS("Ceci est un test de TTS", "fr");
+            await sayTTS("Deuxième message à la suite", "fr");
+            setTimeout(function() {
+                await sayTTS("Troisième message 5 secondes après.");
+            }, 5000);
 
-                    })
-                    .catch((error) => {
-                        console.log("Erreur: " + error)
-                        return 'Il y a eu une erreur';
-                    })
-        */
-        await sayTTS("Ceci est un test de TTS", "fr");
-        await sayTTS("Deuxième message à la suite", "fr");
-        setTimeout(function() {
-            await sayTTS("Troisième message 5 secondes après.");
-        }, 5000);
-
+        })
     })
-})
+}
+
+RunMe();
