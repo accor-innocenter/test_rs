7298f0ccc46ccd447af8a1f09edf1bce711abfa5
cesarmiguel85 <cesarmiguel85@gmail.com>
2019-08-30 17:33:08 +0200

    Update action-rsjs.js

--- action-rsjs.js
+++ action-rsjs.js
@@ -28,18 +28,16 @@ function sayTTS(msg, lang) {
             "lang": lang,
             "siteId": "default"
         }));
-        resolve("ok");
 
-        /*
-                var finished = sayClient.subscribe('hermes/tts/sayFinished');
-                sayClient.on('message', (topic, message) => {
-                    console.log(topic);
-                    console.log(message);
-                    sayClient.unsubscribe('hermes/tts/sayFinished');
-                    resolve(message);
-                });
+        var finished = sayClient.subscribe('hermes/tts/sayFinished');
+        sayClient.on('message', (topic, message) => {
+            console.log(topic);
+            console.log(message);
+            sayClient.unsubscribe('hermes/tts/sayFinished');
+            resolve(message);
+        });
 
-         */
+
     });
 
 }
@@ -64,132 +62,89 @@ function Send(msg, lang) {
 
 }
 
-async function RunMe() {
-    withHermes(async hermes => {
-        // Instantiate a dialog object
-        const dialog = hermes.dialog()
+withHermes(async hermes => {
+    // Instantiate a dialog object
+    const dialog = hermes.dialog()
 
 
-        // Subscribes to intent 'myIntent'
-        dialog.flow('AccorInnovationCenter:OrderRS', async(msg, flow) => {
-            // Log intent message
-            console.log(JSON.stringify(msg))
+    // Subscribes to intent 'myIntent'
+    dialog.flow('AccorInnovationCenter:OrderRS', async(msg, flow) => {
+        // Log intent message
+        console.log(JSON.stringify(msg))
 
 
-            flow.continue('AccorInnovationCenter:FirstCourse', (msg, flow) => {
-                console.log(JSON.stringify(msg));
+        flow.continue('AccorInnovationCenter:FirstCourse', (msg, flow) => {
+            console.log(JSON.stringify(msg));
 
-                const aux = msg.slots.find(slot => slot.slotName === 'Dish1');
-                this.FirstDish = aux.value.value;
+            const aux = msg.slots.find(slot => slot.slotName === 'Dish1');
+            this.FirstDish = aux.value.value;
 
-                flow.continue('AccorInnovationCenter:SecondCourse', (msg, flow) => {
+            flow.continue('AccorInnovationCenter:SecondCourse', (msg, flow) => {
+                console.log(JSON.stringify(msg))
+
+                const aux = msg.slots.find(slot => slot.slotName === 'Dish2');
+                this.SecondDish = aux.value.value;
+
+
+                flow.continue('AccorInnovationCenter:Dessert', (msg, flow) => {
                     console.log(JSON.stringify(msg))
 
-                    const aux = msg.slots.find(slot => slot.slotName === 'Dish2');
-                    this.SecondDish = aux.value.value;
+                    const aux = msg.slots.find(slot => slot.slotName === 'Dessert');
+                    this.Dessert = aux.value.value;
 
 
-                    flow.continue('AccorInnovationCenter:Dessert', (msg, flow) => {
-                        console.log(JSON.stringify(msg))
-
-                        const aux = msg.slots.find(slot => slot.slotName === 'Dessert');
-                        this.Dessert = aux.value.value;
-
-                        /*
-                        let url = server + "domotics/fitness";
-                        var payload = { };
-                        var data = new FormData();
-                        data.append( "json", JSON.stringify( payload ) );
-                        fetch(url,
-                        {
-                            method: "GET",
-                            body: data
-                        })
-                        .then((res)=>{ 
-                            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                            console.log(res);
-                        })
-                        .then((data)=>{ 
-                            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                            console.log(data);
-                        })
-    
-    */
-                        // End the session
-                        flow.end();
 
 
-                        return msg.slots[0].value.value + " est une spécialité de la maison. Très bien, votre commande arrivera dans 20 minutes."
+                    // End the session
+                    flow.end();
 
 
-                    });
+                    return msg.slots[0].value.value + " est une spécialité de la maison. Très bien, votre commande arrivera dans 20 minutes."
 
-                    return msg.slots[0].value.value + ", parfait. Et quel dessert souhaitez-vous déguster?"
 
                 });
 
-                return msg.slots[0].value.value + ", excellent choix. Quel sera votre plat principal?"
+                return msg.slots[0].value.value + ", parfait. Et quel dessert souhaitez-vous déguster?"
 
             });
 
-            /*
-                    let url = server + "domotics/menu";
-                    var payload = { };
-                    var data = new FormData();
-                    data.append( "json", JSON.stringify( payload ) );
-                    fetch(url,
-                    {
-                        method: "GET",
-                        body: data
-                    })
-                    .then((res)=>{ 
-                        console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                        console.log(res);
-                    })
-                    .then((data)=>{ 
-                        console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                        console.log(data);
-                    })
-            */
+            return msg.slots[0].value.value + ", excellent choix. Quel sera votre plat principal?"
 
-            const fetchPromise = fetch(server + "domotics/menu");
-            fetchPromise.then(response => {
-                console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                console.log(response);
-                return response.json();
-            }).then(data => {
-                console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-                console.log(data);
+        });
+
+
+        //SHOW MENU ON SCREEN
+        const fetchPromise = fetch(server + "domotics/menu");
+        fetchPromise.then(response => {
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+            console.log(response);
+            return response.json();
+        }).then(data => {
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@");
+            console.log(data);
+        });
+
+
+        // Use text to speech
+
+        flow.end();
+
+        sayTTS("Ceci est un test de TTS", "fr")
+            .then((data) => {
+                console.log("C'est bon: " + data);
+
+                sayTTS("Deuxième message à la suite", "fr")
+                    .then((data) => {
+                        console.log("C'est bon 2e: " + data);
+                        setTimeout(function() {
+                            sayTTS("Troisième message 5 secondes après.")
+                        }, 5000)
+                    })
+
             });
 
-            // Use text to speech
 
-            flow.end();
-            /*
-                    sayTTS("Ceci est un test de TTS", "fr")
-                        .then((data) => {
-                            console.log("C'est bon: " + data);
-    
-                            sayTTS("Deuxième message à la suite", "fr").then((data) => {
-                                console.log("C'est bon 2e: " + data);
-                                setTimeout(function() {
-                                    sayTTS("Troisième message 5 secondes après.")
-                                }, 5000)
-                            })
-    
-                        })
-                        .catch((error) => {
-                            console.log("Erreur: " + error)
-                            return 'Il y a eu une erreur';
-                        })
-            */
+        return true;
 
-
-            await Send("Ceci est un test de TTS", "fr");
-            await Send("Deuxième message à la suite", "fr");
-
-        })
     })
-}
-
-RunMe();
+})
